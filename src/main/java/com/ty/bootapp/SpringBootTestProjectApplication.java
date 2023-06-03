120351756c01f98cc59f7e41953e8dc71f4faf55
vijaykumararik <vijaykumararikatti@gmail.com>
2023-06-03 11:35:16 +0530

    done

---
+++ src/main/java/com/ty/bootapp/SpringBootTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ty.bootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ty/bootapp/SpringBootTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.bootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
